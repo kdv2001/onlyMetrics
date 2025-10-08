bd1f7f2b362cd1a94c571fd590658e382033edec
dklimovskiy <dklimovskiy@ozon.ru>
2025-10-08 21:46:57 +0300

    iter 20 add build flags

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
 )
 
+var buildVersion string
+var buildDate string
+var buildCommit string
+
+const na = "N/A"
+
 func main() {
+	fmt.Printf("Build version: %s\n", opIf(buildVersion != "", buildVersion, na))
+	fmt.Printf("Build date: %s\n", opIf(buildDate != "", buildDate, na))
+	fmt.Printf("Build commit: %s\n", opIf(buildCommit != "", buildCommit, na))
+
 	if err := initService(); err != nil {
 		log.Fatalf("failed to initialize service: %v", err)
 	}
+}
+
+func opIf[T comparable](cond bool, a T, b T) T {
+	if cond {
+		return a
+	}
+
+	return b
 }
